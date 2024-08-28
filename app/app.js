21f01811c9a9f835cf0b6ec78f8db409df14935b
Evan <158116844+Gnauk841026@users.noreply.github.com>
2024-08-28 10:37:52 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
       </style>
     </head>
     <body>
-      Hello from Cloud Build! Bad !
+      Hello from Cloud Build! Bad ! Bad !
     </body>
     </html>
   `);
